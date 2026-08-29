a49c092386468f859336fdcd6fd11c30f676432b
alvarovillacotarra-web <alvaro.villa.cotarra@gmail.com>
2026-08-29 21:23:00 +0200

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.firebaseConfig = {
-  apiKey: "AIzaSyBNmA5qgQekPMcDqvSQhpjSVaz0Qte0mic",
-  authDomain: "webhook-ee435.firebaseapp.com",
-  projectId: "webhook-ee435",
-  storageBucket: "webhook-ee435.firebasestorage.app",
-  messagingSenderId: "434614628407",
-  appId: "1:434614628407:web:6fc18eea55d76631788fb2",
-  measurementId: "G-6ZT8M9G34L"
-};
